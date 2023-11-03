bc92b88fb353faeffebfeefa291f7a1c95ea6a88
Kabita Joshi <kabitajoshi49@gmail.com>
2023-11-03 10:55:12 +0545

    datatypes assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -4,3 +4,10 @@ let population = 37;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof country);
