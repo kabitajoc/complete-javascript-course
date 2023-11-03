4b2d19a1085275157c74b99e8046a259074b0958
Kabita Joshi <kabitajoshi49@gmail.com>
2023-11-03 17:36:35 +0545

    if else challenge completed

--- 01-Fundamentals-Part-1/challenges/challenge1.js
+++ 01-Fundamentals-Part-1/challenges/challenge1.js
@@ -1,10 +1,17 @@
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
-let BMIMark = massMark / (heightMark * heightMark);
-let BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark);
-console.log(BMIJohn);
-let markHigherBMI = BMIMark > BMIJohn;
-console.log(markHigherBMI);
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+if(BMIMark > BMIJohn){
+console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}`)
+ 
+}else{
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`)
+}
